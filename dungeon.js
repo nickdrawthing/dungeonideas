229094eed5f6c7a1345bd89c57eb11b9b13204b6
nickdrawthing <nicholas.a.h.hendriks@gmail.com>
2019-01-02 12:40:43 -0500

    modified auth method to exclude from repo

--- dungeon.js
+++ dungeon.js
@@ -1,13 +1,8 @@
 var Twit = require('./node_modules/twit');
  
-var T = new Twit({
-  consumer_key:         'N13WiJKUKOIDXVnefYTOT78lW',
-  consumer_secret:      '8ax5WbnibSFF6QsOLL571fEWmduzWmWd5JZuuAj9ClOix9C8Ar',
-  access_token:         '959841386198654981-TenxbBXZZjLlEItXHWm7EIeJ4hIkJbY',
-  access_token_secret:  'mokHrsC6VOFDB7H17fOqCSywvVWAjTpHhCl79fHhNaOn8',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
+var Auth = require('./dungeonIdeasAuth.js');
+
+var T = new Twit(Auth);
 
 var overallDescriptions = [
 	"Arched Brick Tunnels"
@@ -563,5 +558,5 @@ var minute = second*60;
 var hour = minute * 60;
 var day = hour * 24;
 
-// main();
+main();
 setInterval(main,hour * 6); // creates a new one every minute
