378f7c2e2d9899a1d134c81b00274ed06d997385
nickdrawthing <nicholas.a.h.hendriks@gmail.com>
2019-01-02 12:47:27 -0500

    Delete dungeonIdeasAuth.js
    wtf gitignore didn't ignore this as anticipated

--- dungeonIdeasAuth.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  consumer_key:         'N13WiJKUKOIDXVnefYTOT78lW',
-  consumer_secret:      '8ax5WbnibSFF6QsOLL571fEWmduzWmWd5JZuuAj9ClOix9C8Ar',
-  access_token:         '959841386198654981-TenxbBXZZjLlEItXHWm7EIeJ4hIkJbY',
-  access_token_secret:  'mokHrsC6VOFDB7H17fOqCSywvVWAjTpHhCl79fHhNaOn8',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-  strictSSL:            true,     // optional - requires SSL certificates to be valid.
-}
